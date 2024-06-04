f61f6bb7237c6b3de3fd18d2df06843bbd8541ba
useryesa9@gmail.com <useryesa9@gmail.com>
2024-06-04 09:47:46 +0700

    membuat beranda controller dan memberikan komentar di file web.route

--- resources/views/frontend/components/header.blade.php
+++ resources/views/frontend/components/header.blade.php
@@ -16,7 +16,7 @@
 
         <nav id="nav"
             class="absolute top-0 left-0 z-50 flex flex-col items-center justify-between hidden w-full h-64 pt-5 mt-24 text-sm text-gray-800 bg-white border-t border-gray-200 md:w-auto md:flex-row md:h-24 lg:text-base md:bg-transparent md:mt-0 md:border-none md:py-0 md:flex md:relative">
-            <a href="{{ url('/') }}"
+            <a href="{{ route('beranda.index') }}"
                 class="ml-0 mr-0 font-bold duration-100 md:ml-12 md:mr-3 lg:mr-8 transition-color hover:text-indigo-600">Beranda</a>
             <a href="{{ route('tentang-kami.index') }}"
                 class="mr-0 font-bold duration-100 md:mr-3 lg:mr-8 transition-color hover:text-indigo-600">Tentang Kami</a>
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,24 @@
 <?php
 
+use App\Http\Controllers\BerandaController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KontakController;
 use App\Http\Controllers\LayananController;
 use App\Http\Controllers\PengacaraController;
 use App\Http\Controllers\TentangKamiController;
 
-Route::get('/', function () {
-    return view('frontend.pages.index');
-});
-Route::get('/kontak', [KontakController::class, 'index'])->name('kontak.index');
+// Route Beranda
+Route::get('/', [BerandaController::class, 'index'])->name('beranda.index');
 
-Route::resource('layanan', LayananController::class);
+// Route Tentang Kami
 Route::resource('tentang-kami', TentangKamiController::class);
+
+// Route Layanan
+Route::resource('layanan', LayananController::class);
+
+// Route Pengacara
 Route::resource('pengacara', PengacaraController::class);
+
+// Route Kontak
+Route::get('/kontak', [KontakController::class, 'index'])->name('kontak.index');
 
