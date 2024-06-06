ef6aa0821df4d41a04b109151cfea2f652218647
useryesa9@gmail.com <useryesa9@gmail.com>
2024-06-06 12:59:06 +0700

    mengganti struktur data, karna ingin cepat cepatan

--- resources/views/components/form-input.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="md:col-span-5 mb-4">
-    <label for="full_name">{{ $label }}</label>
-    <input type="{{ $type }}" name="{{ $name }}" id="full_name" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" value="" placeholder="{{ $placeholder }}"/>
-</div>
